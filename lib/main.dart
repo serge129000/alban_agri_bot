73023ca52b7ff7fd71ddb970e7481bdaa8615e33
serge129000 <sergegaba8.sg@gmail.com>
2023-03-08 23:58:04 +0000

    flutter_blue commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:agro_bot/Bluetooth.dart';
 import 'package:agro_bot/bluetooth_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_blue/flutter_blue.dart';
 void main() {
   runApp(const MyApp());
 }
